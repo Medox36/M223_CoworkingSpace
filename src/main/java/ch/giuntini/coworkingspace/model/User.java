50ec1ac78a9043ca3652fca196beea5dc2b70605
Medox36 <lorenzo@giuntini.ch>
2023-07-13 11:02:54 +0200

    optimized imports of User.java

--- src/main/java/ch/giuntini/coworkingspace/model/User.java
+++ src/main/java/ch/giuntini/coworkingspace/model/User.java
@@ -1,6 +1,14 @@
 package ch.giuntini.coworkingspace.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.hibernate.annotations.ColumnDefault;
