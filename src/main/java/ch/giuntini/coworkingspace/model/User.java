db1e2d0c73440b6ff68dac4c701f21a8a144095f
Medox36 <lorenzo@giuntini.ch>
2023-07-13 09:54:33 +0200

    added classes related to user

---
+++ src/main/java/ch/giuntini/coworkingspace/model/User.java
@@ -0,0 +1,95 @@
+package ch.giuntini.coworkingspace.model;
+
+import javax.persistence.*;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity(name = "application_user")
+@NamedQueries(value = { 
+    @NamedQuery(name = "User.findByEmail", query = "SELECT u FROM application_user AS u WHERE u.email = :email") 
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false, length = 255, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 32)
+    private String password;
+
+    @Column(length = 127)
+    private String firstName;
+
+    @Column(length = 127)
+    private String lastName;
+
+    @Column(nullable = false, length = 32)
+    private String salt;
+
+    @Column(nullable = false, length = 7)
+    @ColumnDefault(value = "'VISITOR'")
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSalt() {
+        return this.salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public UserRole getRole() {
+        return this.role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }    
+}
---
+++ src/main/java/ch/giuntini/coworkingspace/model/UserRole.java
@@ -0,0 +1,5 @@
+package ch.giuntini.coworkingspace.model;
+
+public enum UserRole {
+    VISITOR, MEMBER, ADMIN
+}
